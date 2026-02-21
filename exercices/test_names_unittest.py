d071cfe95193f5264b26f9c5f187668b66d5dbba
OnePieceisRealdude <zainab.bou2003@gmail.com>
2026-02-21 15:57:15 +0000

    refactor: clean code for names() (tests passing)

--- exercices/test_names_unittest.py
+++ exercices/test_names_unittest.py
@@ -1,25 +1,14 @@
 import unittest
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+MIN_LENGTH = 7
 
+def names(prenoms: list[str]) -> int:
+    return sum(1 for prenom in prenoms if len(prenom) > MIN_LENGTH)
 
 class TestNamesMethod(unittest.TestCase):
     def test_names(self):
         prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+        self.assertEqual(names(prenoms), 4)
 
-
-if names == 'main':
+if __name__ == "__main__":
     unittest.main()
