fcca715a4df28747a89d548a628613fd211c08d9
OnePieceisRealdude <zainab.bou2003@gmail.com>
2026-04-04 00:28:30 +0200

    feat: complete part 5 notebook training workflow and unit test

--- kto-titanic/src/titanic/training/steps/load_data.py
+++ kto-titanic/src/titanic/training/steps/load_data.py
@@ -1,6 +1,9 @@
 import logging
-# TODO : Intégrer les imports manquants
+import os
+from pathlib import Path
 
+import boto3
+import pandas as pd
 
 ARTIFACT_PATH = "path_output"
 PROFILING_PATH = "profiling_reports"
@@ -9,8 +12,19 @@ PROFILING_PATH = "profiling_reports"
 def load_data(path: str) -> str:
     logging.warning(f"load_data on path : {path}")
 
-    # TODO : Intégrer le corps de cette fonction développée dans le notebook
+    Path("./dist/").mkdir(parents=True, exist_ok=True)
+    local_path = Path("./dist/", "data.csv")
+    logging.warning(f"to path : {local_path}")
 
-    # TODO : Dans un second temps, ajouter les logs mlflow, notamment les artifacts du profiling
-    # Mais aussi logger l'artifact du fichier csv.
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=os.environ.get("MLFLOW_S3_ENDPOINT_URL"),
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    )
 
+    s3_client.download_file("kto-titanic", path, local_path)
+    df = pd.read_csv(local_path)
+    logging.warning(f"data shape : {df.shape}")
+
+    return str(local_path)
--- kto-titanic/src/titanic/training/steps/train.py
+++ kto-titanic/src/titanic/training/steps/train.py
@@ -1,15 +1,28 @@
 import logging
-# TODO : Intégrer les imports manquants
+from pathlib import Path
 
-# TODO : Dans une second temps, récupérer le client mlflow nous permettant de télécharger les artifacts enregistrés à l'étape précédente
+import joblib
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 ARTIFACT_PATH = "model_trained"
 
 
 def train(x_train_path: str, y_train_path: str, n_estimators: int, max_depth: int, random_state: int) -> str:
     logging.warning(f"train {x_train_path} {y_train_path}")
+    x_train = pd.read_csv(x_train_path, index_col=False)
+    y_train = pd.read_csv(y_train_path, index_col=False)
 
-    # TODO : Implémenter la fonction avec les expérimentations du notebook
-    # TODO : Dans un second temps, récupérer les données depuis mlflow
+    x_train = pd.get_dummies(x_train)
 
-    # TODO : Dans un second temps, stocker le model en tant qu'artifact dans mlflow
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        random_state=random_state
+    )
+    model.fit(x_train, y_train)
+
+    model_path = Path("./dist/", "model.joblib")
+    joblib.dump(model, model_path)
+
+    return str(model_path)
